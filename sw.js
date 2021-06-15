bd18ab9042666d62753452612ddf6cf628c98458
Allegedly Samuel Casey <76224164+Jeve-Stobs@users.noreply.github.com>
2021-06-15 00:02:15 -0400

    added katex to sw

--- sw.js
+++ sw.js
@@ -74,6 +74,20 @@ workbox.routing.registerRoute(
 );
 workbox.routing.registerRoute(
 	new RegExp(
+		'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.css'
+	),
+	workbox.strategies.cacheFirst({
+		cacheName: 'acoustic-js',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 24 * 60 * 60 * 28,
+				purgeOnQuotaError: !0,
+			}),
+		],
+	})
+);
+workbox.routing.registerRoute(
+	new RegExp(
 		'https://apiv2.popupsmart.com/api/Bundle/script-loader/366405?Referer=https%3A%2F%2Facoustic.to%2F'
 	),
 	workbox.strategies.cacheFirst({
